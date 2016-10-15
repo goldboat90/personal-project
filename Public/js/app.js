9453e7e3d47eaefe75911b7a88863810699e4038
goldboat90 <tyler.rymer@gmail.com>
2016-10-15 17:53:00 -0600

    brought in resolves

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,18 +1,29 @@
 angular.module('overwatchApp', ['ui.router']).config(function ($stateProvider, $urlRouterProvider){
 
-  $stateProvider
-      .state('home', {
-          url: '/',
-          templateUrl: 'js/home/home.html'
-      })
-      .state('heroes', {
-          url: '/heroes',
-          templateUrl: 'js/heroes/heroes.html',
-          controller: 'heroesCtrl'
-      })
-      .state('hero', {
-          url: '/heroes/:heroname',
-          templateUrl: 'js/heroes/singleHero.html',
-          controller: 'singleHeroCtrl'
-      })
+    $stateProvider
+    .state('home', {
+        url: '/',
+        templateUrl: 'js/home/home.html'
+    })
+    .state('heroes', {
+        url: '/heroes',
+        templateUrl: 'js/heroes/heroes.html',
+        controller: 'heroesCtrl'
+    })
+    .state('hero', {
+        url: '/heroes/:heroname',
+        templateUrl: 'js/heroes/singleHero.html',
+        controller: 'singleHeroCtrl'
+        resolve: {
+            hero: function (heroesService, $state) {
+                return heroesService.getHeroes().then(function(res){
+                    for (var i = 0; i < res.data.length; i++) {
+                        if (res.data[i].heroname === $state.params.heroname) {
+                            return res.data[i];
+                        }
+                    };
+                });
+            }
+        }
+    })
 })
